bc811693ccb7ab85a93b3e8bccfe97d08fe44d84
Nykolle Malone <malone.nykolle@gmail.com>
2021-04-25 08:14:25 -0300

    miragejs and axios added

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api("/transactions").then((response) => console.log("console.log", response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -15,17 +22,13 @@ export function TransactionsTable() {
 
         <tbody>
           <tr>
-            <td>
-              Desenvolvimento de website
-            </td>
+            <td>Desenvolvimento de website</td>
             <td className="deposit">R$12.000</td>
             <td>Desenvolvimento</td>
             <td>20/02/2021</td>
           </tr>
           <tr>
-            <td>
-              Aluguel
-            </td>
+            <td>Aluguel</td>
             <td className="withdraw">- R$1.000</td>
             <td>Casa</td>
             <td>17/02/2021</td>
@@ -33,5 +36,5 @@ export function TransactionsTable() {
         </tbody>
       </table>
     </Container>
-  )
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {  createServer } from 'miragejs'
 
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api' // todas as chamadas que eu vou fazer, serão a partir desse endereço 3333/api/transactions por exemplo
+  
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
